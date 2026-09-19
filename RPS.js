dd67ec394aa3febf6b4674648dfd05a701b3f3ab
LeicaCielle <leicacielle@gmail.com>
2026-09-19 23:36:15 +0700

    Remove if in line 25 and fix indentation

--- RPS.js
+++ RPS.js
@@ -1,30 +1,29 @@
 function getComputerChoice() {
-    const choice = ["rock" , "paper" , "scissors"];
+    const choice = ["rock", "paper", "scissors"];
     let calculation = choice[Math.floor(Math.random() * choice.length)];
     console.log("computer answer: " + calculation)
     return calculation;
 }
 
-function singleRound (playerSelection, computerSelection) {
+function singleRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors" || playerSelection.toLowerCase() == "scissors" && computerSelection == "paper" || playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
-        playerScore++;    
+        playerScore++;
         return "You win, Congratulations";
     } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper" || playerSelection.toLowerCase() == "scissors" && computerSelection == "rock" || playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
-        computerScore++;    
+        computerScore++;
         return "You lose, Better luck next time";
     } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "scissors" || playerSelection.toLowerCase() == "rock" && computerSelection == "rock" || playerSelection.toLowerCase() == "paper" && computerSelection == "paper") {
-        draw++;    
+        draw++;
         return "You draw, Try again";
     } else {
-            return "Undefined, Try again";    
+        return "Undefined, Try again";
     }
 }
 
 function game(playerSelection) {
-        const computerSelection = getComputerChoice();
-        if (true) {
-                alert(`The result is ${singleRound(playerSelection, computerSelection)}`);    
-        }   
+    const computerSelection = getComputerChoice();
+    alert(`The result is ${singleRound(playerSelection, computerSelection)}`);
+
 }
 
 let playerScore = 0
@@ -32,8 +31,8 @@ let computerScore = 0
 let draw = 0
 
 for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock Paper Scissors, what's your choice?");
-        game(playerSelection);
+    let playerSelection = prompt("Rock Paper Scissors, what's your choice?");
+    game(playerSelection);
 }
 
 console.log("computerScore " + computerScore)
